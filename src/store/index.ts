82fc979a3062755de68aba58d8390be6f15e7d27
KonstantinosKoulaxizis <kkoulaxizis07@gmail.com>
2022-04-26 14:58:05 +0200

    feat: Add create security btn; feat: Move Home views logic to vuex ( state, fetch requests )

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,84 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import { TableData } from '@/types/types'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
-});
+  state: {
+    loading: false,
+    homeTableData: <TableData[]>[]
+  },
+  getters: {
+    loading: state => state.loading,
+    homeTableData: state => state.homeTableData
+  },
+  mutations: {
+    setTableData(state, data: TableData[]) {
+      state.homeTableData = data
+    },
+    setLodaing(state, isLoading: boolean) {
+      state.loading = isLoading
+    }
+  },
+  actions: {
+    async handleGetData({ commit, dispatch }): Promise<void> {
+      try {
+        commit('setLodaing', true)
+        const data = await dispatch('getData')
+        const tableData = data.map((dataItem: TableData) => {
+          return {
+            ...dataItem,
+            randomNumber: Math.random()
+          }
+        })
+        commit('setTableData', tableData)
+        commit('setLodaing', false)
+      } catch (error) {
+        console.log(error, 'This is not good')
+      }
+    },
+
+    async getData(): Promise<TableData[]> {
+      return [
+        {
+          id: '42f2462d-49d0-4e91-8fe1-de2e656b0f06',
+          name: 'Series A',
+          nominalValue: 5,
+          authorizedAmount: 1500,
+          issuedAmount: 500,
+          authorizedCapital: 7550,
+          issuedCapital: 2500
+        },
+        {
+          id: '42f2462d-49d0-4e91-8fe1-de2e656b0f06',
+          name: 'Series B',
+          nominalValue: 10,
+          authorizedAmount: 15000,
+          issuedAmount: 5000,
+          authorizedCapital: 150000,
+          issuedCapital: 50000
+        },
+        {
+          id: 'fd78c11b-e3d2-455a-99b0-49907a75c463',
+          name: 'Series C',
+          nominalValue: 1,
+          authorizedAmount: 96876,
+          issuedAmount: 61760,
+          authorizedCapital: 96876,
+          issuedCapital: 61760
+        },
+        {
+          id: 'd8654cb0-8986-4fbc-b969-025e514cb934',
+          name: 'Series D',
+          nominalValue: 1,
+          authorizedAmount: 10110,
+          issuedAmount: 1100,
+          authorizedCapital: 10110,
+          issuedCapital: 1100
+        }
+      ]
+    }
+  },
+  modules: {}
+})
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="home">
     <h1>This is a table with some important data</h1>
-    <b-table :data="tableData" :columns="columns"></b-table>
-    <table-totals :tableData="tableData" />
+    <div id="add-security-container">
+      <button>Add security</button>
+    </div>
+    <b-table :data="$store.state.homeTableData" :columns="columns"></b-table>
+    <table-totals :tableData="$store.state.homeTableData" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import { TableData } from "@/types/types";
 import TableTotals from "@/components/tableTotals.vue";
 import { HOME_TABLE_NUM_COLUMNS } from "@/classes/TableTotalsData";
 
@@ -16,7 +18,6 @@ import { HOME_TABLE_NUM_COLUMNS } from "@/classes/TableTotalsData";
   components: { TableTotals },
 })
 export default class Home extends Vue {
-  tableData: TableData[] = [];
   columns = [
     {
       label: "Security class",
@@ -24,69 +25,18 @@ export default class Home extends Vue {
     },
     ...HOME_TABLE_NUM_COLUMNS,
   ];
-  loading = false;
 
-  // mounted works fine if your ide complains about it
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  mounted() {
-    this.handleGetData();
+  // Add missing return type of function in order to remove editor's complains
+  mounted(): void {
+    this.handleGetInitialData();
   }
 
-  async handleGetData(): Promise<void> {
-    try {
-      this.loading = true;
-      const data = await this.getData();
-      this.tableData = data.map((dataItem: TableData) => {
-        return {
-          ...dataItem,
-          randomNumber: Math.random(),
-        };
-      });
-      this.loading = false;
-    } catch (error) {
-      console.log(error, "This is not good");
+  async handleGetInitialData(): Promise<void> {
+    // Implement a check and fetch data only if the array is empty
+    // in order to prevent unnecessary requests
+    if (!this.$store.state.homeTableData) {
+      await this.$store.dispatch("handleGetData");
     }
-  }
-
-  async getData(): Promise<TableData[]> {
-    return [
-      {
-        id: "42f2462d-49d0-4e91-8fe1-de2e656b0f06",
-        name: "Series A",
-        nominalValue: 5,
-        authorizedAmount: 1500,
-        issuedAmount: 500,
-        authorizedCapital: 7550,
-        issuedCapital: 2500,
-      },
-      {
-        id: "42f2462d-49d0-4e91-8fe1-de2e656b0f06",
-        name: "Series B",
-        nominalValue: 10,
-        authorizedAmount: 15000,
-        issuedAmount: 5000,
-        authorizedCapital: 150000,
-        issuedCapital: 50000,
-      },
-      {
-        id: "fd78c11b-e3d2-455a-99b0-49907a75c463",
-        name: "Series C",
-        nominalValue: 1,
-        authorizedAmount: 96876,
-        issuedAmount: 61760,
-        authorizedCapital: 96876,
-        issuedCapital: 61760,
-      },
-      {
-        id: "d8654cb0-8986-4fbc-b969-025e514cb934",
-        name: "Series D",
-        nominalValue: 1,
-        authorizedAmount: 10110,
-        issuedAmount: 1100,
-        authorizedCapital: 10110,
-        issuedCapital: 1100,
-      },
-    ];
   }
 }
 </script>
